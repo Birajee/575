cf745b9d4c7545ea0ac59ecf713fd019c3e25412
Birajee <95649772+Birajee@users.noreply.github.com>
2024-01-30 15:04:37 +0000

    Pending changes exported from your codespace

---
+++ .vscode/hello.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{ 
+    cout << "Description:\n ";
+    cout << "ET-575 is a course in procedural programming and algorithmic" <<endl;
+    cout << "concepts using the C++ language. \n";
+    cout << "ET-575 Class Schedule:" <<endl;
+
+    cout << "\nTime\t\tLocation\n";
+    cout << "-------\t\t-------- \n";
+    cout << "9:00 AM\t\tRoom T18 \n";
+
+
+return 0;
+
+
+} // namespace std;
